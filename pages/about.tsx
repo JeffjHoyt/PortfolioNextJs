ecd25af82d877c500ac0a2fd2e4557d85010dda0
Jeff Hoyt <JeffjHoyt@gmail.com>
2023-02-10 11:04:09 -0500

    fingers crossed.........

---
+++ pages/about.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Container, Row, Col, Nav, Navbar } from "react-bootstrap";
+import Link from "next/link";
+import { Inter } from "@next/font/google";
+import Script from "next/script";
+import Head from "next/head";
+import About from "../my-portfolio/components/About";
+
+const about = () => {
+  return (
+    <>
+      <Row
+        style={{
+          gap: "20px",
+          width: "100vw",
+          paddingLeft: "0 !important",
+          paddingRight: "0 !important",
+        }}
+      >
+        <Col
+          style={{
+            gap: "20px",
+            paddingLeft: "0 !important",
+            paddingRight: "0 !important",
+          }}
+        >
+          <About />
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default about;
