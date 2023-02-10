50e367ac2f76eb86620756ddb1737736bbe79c85
Jeff Hoyt <JeffjHoyt@gmail.com>
2023-02-10 09:40:02 -0500

    ag

--- my-portfolio/next.config.js
+++ my-portfolio/next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.imgur.com",
-        port: "443",
-        pathname: "/**/*",
-      },
-    ],
-  },
 };
 
-module.exports = () => {
-  // Run the base config through any configured plugins
-  return Object.values(plugins).reduce(
-    (acc, plugin) => plugin(acc),
-    nextConfig
-  );
-};
+module.exports = nextConfig;
