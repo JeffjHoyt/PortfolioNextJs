1551f9c90f05e0af69586fe5fcbe13f0e2915e15
Jeff Hoyt <JeffjHoyt@gmail.com>
2023-04-11 09:26:30 -0400

    meta cont...

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -11,7 +11,7 @@ const Resume = () => {
     <>
       <Head>
         <title>About Me</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
         <link rel="icon" href="/favicon.ico" />
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
