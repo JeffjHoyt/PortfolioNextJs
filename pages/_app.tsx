0e39be5ea5369e6db0a4ef3c1d153ce13f72d30b
Jeff Hoyt <JeffjHoyt@gmail.com>
2023-04-13 10:51:39 -0400

    app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -42,6 +42,7 @@ export default function App({ Component, pageProps }: AppProps) {
                 zIndex: "100",
                 marginBottom: "60px",
                 backgroundColor: "#2C3D55 !important",
+                // hi
               }}
             >
               <Container>
