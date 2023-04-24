dc56b1922870aee9ffd1e11294d0dd6253e183bd
Jeff Hoyt <JeffjHoyt@gmail.com>
2023-04-24 10:48:17 -0400

    message app

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "70bae5df4de4bb6e"; }
+/******/ 		__webpack_require__.h = function() { return "4d415dfb6ef284c9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ .next/static/webpack/pages/index.0f67cb0518e9917e.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./components/ProjectList.json":
+/*!*************************************!*\
+  !*** ./components/ProjectList.json ***!
+  \*************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"projects":[{"name":"Jammin","id":"1","description":"This project was built using React.js. Jammin is a Spotify like app that allows users to search for music, preview songs, add tracks to their playlist, and save them to their own Spotify account.","image":"https://i.imgur.com/CsXmFaf.jpg","alt":"jammin project","src":"https://jeffjhoyt.github.io/jammming/","tools":["React.js",", Spotify API",", Javascript",", CSS",", HTML"]},{"name":"Weather Services","id":"14","description":"This project was built with React.js and utilized the OpenWeatherMap API. It allows users to search for weather information by zip code to get todays weather along with a 5 day forecast.","image":"https://i.imgur.com/DfaTxOh.jpg","alt":"jammin project","src":"https://jeffjhoyt.github.io/weather-services/","tools":["React.js",", OpenWeatherMap API",", Javascript",", CSS",", HTML"]},{"name":"Google clone","id":"2","description":"In this project, I took on the challenge of recreating Google\'s homepage with HTML, CSS, and Javascript.","image":"https://i.imgur.com/5IEl0Dl.jpg","src":"https://jeffjhoyt.github.io/projects/googleClone/index.html","tools":["HTML",", CSS",", Javascript"]},{"name":"Messaging app","id":"7","description":"This project was built purely with react js. It allows the user to send text messages back. ","image":"https://i.imgur.com/xwGZttE.png","src":"https://jeffjhoyt.github.io/Messaging/","tools":["React, CSS"]},{"name":"Task generator","id":"3","description":"This was a project that myself and 5 other developers designed and coded together. It allows users to add to-do lists and tasks with specific information. ","image":"https://i.imgur.com/pSsy0ex.jpg","src":"https://jeffjhoyt.github.io/JWD-Final-Project/","tools":["HTML",", CSS",", Javascript"]},{"name":"ChoreDoor","id":"4","description":"This project was built using vanilla javascript. It uses some basic logic that randomizes the door that should not be chosen with each new game.","image":"https://i.imgur.com/0HZu9qf.jpg","src":"https://jeffjhoyt.github.io/projects/choredoor/index.html","tools":["HTML",", CSS",", Javascript"]},{"name":"Order summary","id":"5","description":"Frontend mentor challenge that required me to take static images and translate those designs into interactable UI elements.","image":"https://i.imgur.com/uvm4mRo.jpg","src":"https://jeffjhoyt.github.io/projects/order-summary/index.html","tools":["HTML",", CSS",", Javascript"]},{"name":"Budget summary","id":"6","description":"Frontend mentor challenge that required me to take static images and translate those designs into interactable UI elements.","image":"https://i.imgur.com/DPCTiK4.jpg","src":"https://jeffjhoyt.github.io/projects/expenses-chart-component-main/pj2.html","tools":["HTML",", CSS",", Javascript"]}]}');
+
+/***/ })
+
+});
